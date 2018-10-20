2ab39cc5ca38ef6c37df7b6ad4cac2e6abc1a1ab
Christina Matson <cmatson93@gmail.com>
2018-10-19 22:37:54 -0700

    Initial commit

---
+++ serve.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql')
+const app = express();
+
+const schema = require('./schema.js')
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphql: true
+}))
+app.listen(4000)
+console.log('Listening...')
